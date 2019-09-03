af5934b3c07b60466ac672a1befe58bba71d798a
yonniii <rkdtjdus753@gmail.com>
2019-09-03 15:25:27 +0900

    code to get md5 hash

---
+++ com/md5hash.py
@@ -0,0 +1,12 @@
+import hashlib
+
+class md5hash:
+    def getHash(self,path,blocksize=65536):
+        afile = open(path, 'rb')
+        hasher = hashlib.md5()
+        buf = afile.read(blocksize)
+        while len(buf) > 0:
+            hasher.update(buf)
+            buf = afile.read(blocksize)
+        afile.close()
+        return hasher.hexdigest()

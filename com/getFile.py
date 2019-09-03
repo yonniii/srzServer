ca322d2f003be1d898e74176a9991814bf8301d3
yonniii <rkdtjdus753@gmail.com>
2019-09-03 15:33:47 +0900

    modity metadata set

--- com/getFile.py
+++ com/getFile.py
@@ -1,5 +1,6 @@
 import os
 import datetime
+from md5hash import md5hash
 
 # path_dir = "C:Users/rkdtj/PycharmProjects/unititled/files"
 import string
@@ -11,12 +12,17 @@ class getFiles:
     def exe(self):
         fileList = os.listdir(self.path_dir)
         list = []
+        hash = md5hash()
         if os.path.exists(self.path_dir):
             for i in fileList:
                 path = self.path_dir+"\\"+i
                 fileinfo = os.stat(path)
-                # print("filename :%s size :%d modified :%s" %(i,fileinfo.st_size,datetime.datetime.fromtimestamp(fileinfo.st_mtime)))
-                list.append("filename :%s size :%d modified :%s" %(i,fileinfo.st_size,datetime.datetime.fromtimestamp(fileinfo.st_mtime)))
+                md5 = hash.getHash(path)
+                list.append("%s,%s,%s,%s" %(datetime.datetime.fromtimestamp(fileinfo.st_mtime),
+                                         i,
+                                         path,
+                                         md5))
+                # list.append("filename :%s size :%d modified :%s" %(i,fileinfo.st_size,datetime.datetime.fromtimestamp(fileinfo.st_mtime)))
         return list
 
 g=getFiles()

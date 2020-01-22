3e9e6e16dd8edac35fe8cab6ac4b55cc8776680a
yonniii <rkdtjdus753@gmail.com>
2020-01-21 21:50:05 -0800

    update

--- com/client.py
+++ com/client.py
@@ -11,18 +11,28 @@ class SRZManager:
         g = getFiles()
         list = g.exe()
         for i in list:
-            i="1"+i
-            self.__sock.send(i.encode('utf-8'))
+            data='0'+i
+            self.__sock.send(data.encode('utf-8'))
             time.sleep(1)
-        print('file info를 전송했습니다.')
+        print('Send file info')
 
     def reqMeta(self):
-        reqM = "1"
+        reqM = '1'
         self.__sock.send(reqM.encode('utf-8'))
 
+    def inputCmd(self):
+        print('SADFASDFSA')
+        while True:
+            sendData = input('0:sendMeta / 1:reqMeta / 2+md5 :search / 3+md5:delete / 4+time : delete before\n')
+            if sendData is '0':
+                self.sendMeta()
+            elif sendData is '1':
+                self.reqMeta()
+            else:
+                self.__sock.send(sendData.encode('utf-8'))
 
     def run(self):
-        sender = threading.Thread(target=self.reqMeta)
+        sender = threading.Thread(target=self.inputCmd)
         sender.start()
         sender.join()
 
@@ -54,20 +64,20 @@ class RecoveryManager:
         sender.join()
         receiver.join()
 
-def send(sock):
-    while True:
-        sendData = input('>>>')
-        sock.send(sendData.encode('utf-8'))
+# def send(sock):
+#     while True:
+#         sendData = input('>>>')
+#         sock.send(sendData.encode('utf-8'))
 
-port = 8081
+port = 8080
 
 clientSock = socket(AF_INET, SOCK_STREAM)
 clientSock.connect(('127.0.0.1', port))
 
-print('접속 완료')
+print('connect')
 
-sender = threading.Thread(target=send, args=(clientSock,))
-sender.start()
+# sender = threading.Thread(target=send, args=(clientSock,))
+# sender.start()
 srzmanager = SRZManager(clientSock)
 srzmanager.run()
 recovery = RecoveryManager(clientSock)
--- com/query.py
+++ com/query.py
@@ -5,11 +5,11 @@ from md5hash import md5hash
 class DB:
 
     def __init__(self):
-        self.__db = pymysql.connect(host='localhost',
+        self.__db = pymysql.connect(host='127.0.0.1',
                                     port=3306,
                                     user='root',
-                                    password='1234',
-                                    db='srzserver',
+                                    password='',
+                                    db='srz_db',
                                     charset='utf8')
         self.__cursor = self.__db.cursor()
 
@@ -19,30 +19,30 @@ class DB:
 
     def insert(self, data):
         hash = md5hash()
-        sql = """INSERT INTO `metadatas`(`ctime`, `filename`, `path`, `hash`) VALUES (%s,'%s','%s','%s')"""
+        sql = """INSERT INTO `metadata`(`ctime`, `filename`, `path`, `hash`) VALUES (%s,'%s','%s','%s') ON DUPLICATE KEY UPDATE hash='%s'"""
         values = data.split(',')
         md5 = hash.getHash('%s,%s' % (values[2], values[0]))
-        sql = sql % (values[0], values[1], values[2], md5)
+        sql = sql % (values[0], values[1], values[2], md5, md5)
         print(sql)
         self.__executeQuery(sql)
 
     def serchMD5(self,md5):
-        sql= """SELECT * FROM srzserver.metadatas WHERE hash='%s'"""
+        sql= """SELECT * FROM srz_db.metadata WHERE hash='%s'"""
         sql = sql%(md5)
         self.__executeQuery(sql)
         return self.__cursor.fetchall()
 
     def deleteHash(self, hash):
-        sql = """DELETE FROM metadatas WHERE hash = '%s'""" % (hash)
+        sql = """DELETE FROM metadata WHERE hash = '%s'""" % (hash)
         print(sql)
         self.__executeQuery(sql)
 
     def deleteOld(self, time):
-        sql = """DELETE FROM metadatas where ctime <= %s""" % (time)
+        sql = """DELETE FROM metadata where ctime <= %s""" % (time)
         print(sql)
         self.__executeQuery(sql)
 
     def restore(self):
-        sql = """SELECT * FROM metadatas order by ctime DESC """ # 최근에 생성된 순으로 정렬
+        sql = """SELECT * FROM metadata order by ctime DESC """ # 최근에 생성된 순으로 정렬
         self.__executeQuery(sql)
         return self.__cursor.fetchall()
